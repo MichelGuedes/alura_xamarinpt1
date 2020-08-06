565d71e17c3d9ab54cb79119f12ed6bdd1fa75f8
Michel Guedes <michel.guedes.vieira@gmail.com>
2020-08-06 15:53:50 -0300

    Commit da primeira aula

---
+++ AluraCar/AluraCar/AluraCar/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AluraCar
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            lstVeiculos.ItemsSource = 
+                new string[] {
+                    "Azera 2.0",
+                    "Onix 1.4",
+                    "Uno 1994"
+                };
+        }
+    }
+}
